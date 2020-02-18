074f1f4940f4c16023c2a24baef973d4fdfdd94b
Jason <acinate@hotmail.com>
2020-02-17 21:56:58 -0500

    added navbar and login interfaces

--- src/react/components/app.tsx
+++ src/react/components/app.tsx
@@ -1,12 +1,15 @@
 import {hot} from 'react-hot-loader/root';
 import React from 'react';
 import 'assets/scss/app.scss';
+import Navbar from "./navbar/navbar";
+import Login from "./login/login";
 
 const App = () => {
     return (
         <div className="app_container">
+            <Navbar/>
             <div className="app_inner">
-                <h1>Discover Town!</h1>
+                <Login/>
             </div>
         </div>
     )
---
+++ src/react/components/login/oauthButton.tsx
@@ -0,0 +1,19 @@
+import React from 'React';
+import fbLogo from "../../../assets/img/facebook-logo.png";
+import googleLogo from "../../../assets/img/google-logo.png";
+
+interface Props {
+    platform: string;
+}
+
+export const OAuthButton = (props: Props) => {
+    return <button className={props.platform === "google" ? "btn-google-login" : "btn-facebook-login"}>
+        <div className="logo">
+            <img src={props.platform === "google" ? googleLogo : fbLogo}
+                 alt={props.platform === "google" ? "google" : "facebook"}/>
+        </div>
+        <div className="text">{props.platform === "google" ? "Log in with Google" : "Log in with Facebook"}</div>
+    </button>
+};
+
+export default OAuthButton;
