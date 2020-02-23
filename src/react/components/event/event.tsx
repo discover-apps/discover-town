c2a69e9ca1104730812d5c90ddebf0c9ce1cbb3a
Jason <acinate@hotmail.com>
2020-02-23 18:47:56 -0500

    added event details

--- src/react/components/event/event.tsx
+++ src/react/components/event/event.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import EventTitle from "./eventTitle";
 import EventInformation from "./eventInformation";
+import EventDetails from "./eventDetails";
 
 export const Event = () => {
     return (
         <main className="event">
             <EventTitle/>
             <EventInformation/>
+            <EventDetails/>
         </main>
     )
 };
