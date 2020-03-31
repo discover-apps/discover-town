c3943fe771907f3bcee3ed4ad7e531aefd5ab45d
Jason <acinate@hotmail.com>
2020-03-31 18:11:35 -0400

    added loading spinner to error page

--- src/react/components/error/error.tsx
+++ src/react/components/error/error.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import NoResults from "../../../assets/img/no-results.png";
 import {Link, Redirect} from "react-router-dom";
+import {CircularProgress} from "@material-ui/core";
 
 export const ErrorPage = () => {
+    const [loading, setLoading] = useState(true);
     const [seconds, setSeconds] = useState(5);
     useEffect(() => {
         const interval = setTimeout(() => {
@@ -13,12 +15,18 @@ export const ErrorPage = () => {
     if (seconds <= 0) {
         return <Redirect to={'/'}/>
     }
-    return <main className="error">
-        <div className="image">
-            <img src={NoResults} alt="no_results.png"/>
+    if (loading) {
+        return <div className="loading">
+            <CircularProgress/>
         </div>
-        <h1>404 Page Not Found</h1>
-        <p>Sorry, the requested resource could not be found. You will be redirected in {seconds} seconds.</p>
-        <p><Link to={'/'}>Click here</Link> to redirect now</p>
-    </main>
+    } else {
+        return <main className="error">
+            <div className="image">
+                <img src={NoResults} alt="no_results.png"/>
+            </div>
+            <h1>404 Page Not Found</h1>
+            <p>Sorry, the requested resource could not be found. You will be redirected in {seconds} seconds.</p>
+            <p><Link to={'/'}>Click here</Link> to redirect now</p>
+        </main>
+    }
 };
