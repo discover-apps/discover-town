65dc464fc1b297c7ef9a98fc7d697b99fc4b3648
Jason <acinate@hotmail.com>
2020-04-06 23:19:04 -0400

    refactored searchNearbyPlaces into its own component

--- src/server/api/googlePlace.api.ts
+++ src/server/api/googlePlace.api.ts
@@ -76,3 +76,29 @@ export const getImage = (photoReference: string): Promise<string> => {
         });
     });
 };
+
+export const searchNearbyPlaces = (query: string, maxResults: number): Promise<GooglePlace[]> => {
+    return new Promise<GooglePlace[]>((resolve, reject) => {
+        const key: string = GOOGLE_MAPS_API_KEY;
+        const url: string = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURI(query)}&key=${key}`;
+        axios.get(url).then((response: AxiosResponse) => {
+            maxResults = response.data.results.length < maxResults || maxResults == -1 ? response.data.results.length : maxResults;
+            const results: SearchResult[] = [];
+            // loop through response.data.results
+            for (let i = 0; i < maxResults; i++) {
+                const r = response.data.results[i];
+                if (r.formatted_address && r.name && r.geometry && r.geometry.location && r.geometry.location.lat != undefined && r.geometry.location.lng != undefined) {
+                    results.push({
+                        name: r.name,
+                        formatted_address: r.formatted_address,
+                        lat: r.geometry.location.lat,
+                        lon: r.geometry.location.lng
+                    });
+                }
+            }
+            resolve(results);
+        }).catch((error) => {
+            reject(error);
+        });
+    });
+};
