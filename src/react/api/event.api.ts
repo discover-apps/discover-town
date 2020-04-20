14b90bc2b4b3eb504c7b22872370621e51a5d404
Jason <acinate@hotmail.com>
2020-04-20 17:34:04 -0400

    formatted event.api.ts

--- src/react/api/event.api.ts
+++ src/react/api/event.api.ts
@@ -1,11 +1,11 @@
 import axios, {AxiosResponse} from "axios";
 import moment from "moment";
 import {handleHttpError, http} from "./_api";
-import {Event} from '../models/event.model';
+import {Event} from "../models/event.model";
 import {User} from "../models/user.model";
 
 export const searchPlaces = async (query: string): Promise<any> => {
-    const response: AxiosResponse = await axios.post('/api/event/places', {query});
+    const response: AxiosResponse = await axios.post("/api/event/places", {query});
     if (response.status == 200) {
         return response.data;
     }
@@ -14,13 +14,13 @@ export const searchPlaces = async (query: string): Promise<any> => {
 
 export const createEventRecord = async (event: Event): Promise<Event> => {
     return new Promise<Event>((resolve, reject) => {
-        http.post('/event/create', event).then((response: AxiosResponse<Event>) => {
+        http.post("/event/create", event).then((response: AxiosResponse<Event>) => {
             resolve(response.data);
         }).catch((error) => {
-            if (error != undefined && error.response != undefined && error.response.data != undefined && typeof error.response.data == 'string') {
+            if (error != undefined && error.response != undefined && error.response.data != undefined && typeof error.response.data == "string") {
                 reject(error.response.data);
             } else {
-                reject('An unknown error occurred when creating event record.');
+                reject("An unknown error occurred when creating event record.");
             }
         });
     });
@@ -28,7 +28,7 @@ export const createEventRecord = async (event: Event): Promise<Event> => {
 
 export const updateEventRecord = async (event: Event): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
-        http.post('/event/update', {event}).then((response: AxiosResponse<string>) => {
+        http.post("/event/update", {event}).then((response: AxiosResponse<string>) => {
             resolve(response.data);
         }).catch((error) => {
             reject(handleHttpError(error, "An unknown error occurred when updating event record."));
@@ -38,7 +38,7 @@ export const updateEventRecord = async (event: Event): Promise<string> => {
 
 export const deleteEventRecord = async (event: Event): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
-        http.post('/event/delete', {event}).then((response: AxiosResponse<string>) => {
+        http.post("/event/delete", {event}).then((response: AxiosResponse<string>) => {
             resolve(response.data);
         }).catch((error) => {
             handleHttpError(error, "An unknown error occurred when deleting event.");
@@ -48,7 +48,7 @@ export const deleteEventRecord = async (event: Event): Promise<string> => {
 
 export const readEventById = (eventId: number): Promise<Event> => {
     return new Promise<Event>((resolve, reject) => {
-        http.post('/event/readById', {eventId}).then((response: AxiosResponse<Event>) => {
+        http.post("/event/readById", {eventId}).then((response: AxiosResponse<Event>) => {
             if (response.data.id == undefined) {
                 resolve(undefined);
             } else {
@@ -66,7 +66,7 @@ export const readEventById = (eventId: number): Promise<Event> => {
 
 export const readEventsByUser = (user: User): Promise<Event[]> => {
     return new Promise<Event[]>((resolve, reject) => {
-        http.post('/event/readByUser', user).then((response: AxiosResponse<Event[]>) => {
+        http.post("/event/readByUser", user).then((response: AxiosResponse<Event[]>) => {
             const events: Event[] = [];
             for (let i = 0; i < response.data.length; i++) {
                 events.push({
@@ -77,10 +77,10 @@ export const readEventsByUser = (user: User): Promise<Event[]> => {
             }
             resolve(events);
         }).catch((error) => {
-            if (error != undefined && error.response != undefined && error.response.data != undefined && typeof error.response.data == 'string') {
+            if (error != undefined && error.response != undefined && error.response.data != undefined && typeof error.response.data == "string") {
                 reject(error.response.data);
             } else {
-                reject('An unknown error occurred when retrieving event records for user.');
+                reject("An unknown error occurred when retrieving event records for user.");
             }
         });
     });
@@ -88,7 +88,7 @@ export const readEventsByUser = (user: User): Promise<Event[]> => {
 
 export const readEventsByFollowers = (): Promise<Event[]> => {
     return new Promise<Event[]>((resolve, reject) => {
-        http.post('/event/readByFollowers').then((response: AxiosResponse<Event[]>) => {
+        http.post("/event/readByFollowers").then((response: AxiosResponse<Event[]>) => {
             const events: Event[] = [];
             for (let i = 0; i < response.data.length; i++) {
                 events.push({
@@ -99,47 +99,47 @@ export const readEventsByFollowers = (): Promise<Event[]> => {
             }
             resolve(events);
         }).catch((error) => {
-            reject(handleHttpError(error, 'An unknown error occurred when retrieving event from user followers.'));
+            reject(handleHttpError(error, "An unknown error occurred when retrieving event from user followers."));
         });
     });
 };
 
 export const readEventAttendees = async (event: Event): Promise<User[]> => {
     return new Promise<User[]>((resolve, reject) => {
-        http.post('/event/readAttendees', {event}).then((response: AxiosResponse<User[]>) => {
+        http.post("/event/readAttendees", {event}).then((response: AxiosResponse<User[]>) => {
             resolve(response.data);
         }).catch((error) => {
-            reject(handleHttpError(error, 'An unknown error occurred when retrieving attendees for event.'));
+            reject(handleHttpError(error, "An unknown error occurred when retrieving attendees for event."));
         });
     });
 };
 
 export const createEventAttendee = async (event: Event): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
-        http.post('/event/createAttendee', {event}).then((response: AxiosResponse<string>) => {
+        http.post("/event/createAttendee", {event}).then((response: AxiosResponse<string>) => {
             resolve(response.data);
         }).catch((error) => {
-            reject(handleHttpError(error, 'An unknown error occurred when creating event attendee.'));
+            reject(handleHttpError(error, "An unknown error occurred when creating event attendee."));
         });
     });
 };
 
 export const deleteEventAttendee = async (event: Event): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
-        http.post('/event/deleteAttendee', {event}).then((response: AxiosResponse<string>) => {
+        http.post("/event/deleteAttendee", {event}).then((response: AxiosResponse<string>) => {
             resolve(response.data);
         }).catch((error) => {
-            reject(handleHttpError(error, 'An unknown error occurred when deleting event attendee.'));
+            reject(handleHttpError(error, "An unknown error occurred when deleting event attendee."));
         });
     });
 };
 
 export const userAttendingEvent = async (event: Event): Promise<boolean> => {
     return new Promise<boolean>((resolve, reject) => {
-        http.post('/event/attendingEvent', {event}).then((response: AxiosResponse<boolean>) => {
+        http.post("/event/attendingEvent", {event}).then((response: AxiosResponse<boolean>) => {
             resolve(response.data);
         }).catch((error) => {
-            reject(handleHttpError(error, 'An unknown error occurred when checking if user is attending event.'));
+            reject(handleHttpError(error, "An unknown error occurred when checking if user is attending event."));
         });
     });
 };
